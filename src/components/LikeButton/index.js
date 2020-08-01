57b42a46d76e2e715812b7b53ccb1f065cabdb95
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-08-01 13:24:21 -0400

    ex5 done circle showing behind heart

--- src/components/LikeButton/index.js
+++ src/components/LikeButton/index.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 import Heart from "./Heart";
 import { TweetContext } from '../TweetContext';
+import PoppingCircle from './PoppingCircle';
 
 const PARTICLE_COLORS = ["#e53935", "#1e88e5", "#43a047", "#fdd835", "#fb8c00"];
 
@@ -12,7 +13,8 @@ const LikeButton = ({ size = 40 }) => {
 
   return (
     <Wrapper style={{ width: size, height: size }}>
-      <Heart width={heartSize} isToggled={isLikedByCurrentUser} />
+      {isLikedByCurrentUser && <PoppingCircle size={size} color='#E790F7'/>}
+      <Heart width={heartSize} isToggled={isLikedByCurrentUser}/>
     </Wrapper>
   );
 };
